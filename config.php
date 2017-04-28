0e088fe1e03b2f6597cb16abd39f1cc2bab34ef5
matthew ha <matthew ha>
2017-04-28 05:57:10 -0400

    config edit

--- config.php
+++ config.php
@@ -1,16 +1,22 @@
 <?php
 
 //$ini = parse_ini_file('vendor\test\config.ini');
+$key = getenv('app_pass');
+// print gettype($key);
+// $key2 = json_decode($key,true);
+// print_r($key2);
+// die();
 
 class Configuration {
 
   public $endpoint;
   public $apiKey;
-
+  public $key;
+  
   public function __construct() {
-
+    global $key;
     $this->endpoint = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
-    $this->apiKey = getenv('app_pass');
+    $this->apiKey = $key;
 
   }
 
