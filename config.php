be84987f293c91d1ccff2947b529925307d4d374
Matthew Ha <g2peee@hotmail.com>
2017-04-28 03:15:50 -0400

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ class Configuration {
 
   public function __construct() {
     $this->endpoint = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
-    $this->apiKey = '51a9e2c2b75e477ca2d6e8e6263c23cc';
+    $this->apiKey = '';
   }
 
 }
