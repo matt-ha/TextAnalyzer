f2d752c6ce9ea63caa17e22ad223df08e0477c99
matthew ha <matthew ha>
2017-05-01 23:05:28 -0400

    testing

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 require_once getcwd() . '/vendor/autoload.php';
-// $ini = parse_ini_file('C:/xampp/htdocs/vendor/test/config.ini');
+// $ini = parse_ini_file('vendor/test/config.ini');
 // $key = ($ini['app_pass']);
 
 class Configuration {
@@ -12,7 +12,7 @@ class Configuration {
 
     global $key;
     $this->endpoint = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
-    $this->apiKey = $key;
+    $this->apiKey = 'pass';
 
   }
 
